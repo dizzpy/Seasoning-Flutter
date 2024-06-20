56c16050ca23fbae1e22a332ba428072cf27811e
Anuja Nisal Yashohara <rmanuja2003@gmail.com>
2024-06-20 13:19:39 +0530

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# seasoning_recipe_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/dev/dizzpy/seasoning/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.dizzpy.seasoning
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
