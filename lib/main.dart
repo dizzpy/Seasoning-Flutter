e272df0476e2f845962068b33661f1b5b31de7ab
Anuja Nisal Yashohara <rmanuja2003@gmail.com>
2024-06-20 13:32:53 +0530

    theme update

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Hello Seasoning!'),
+        ),
+      ),
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+          // ···
+          brightness: Brightness.light,
         ),
       ),
     );
